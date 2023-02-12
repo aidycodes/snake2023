a1e6d08fe7a72dd7c4182c5eb4e12a58d26b67a5
Aidy <aidycodes@gmail.com>
2023-02-12 21:06:30 +0000

    bug fixes

--- components/scoreboard/ScoreBoard.tsx
+++ components/scoreboard/ScoreBoard.tsx
@@ -12,7 +12,7 @@ const ScoreBoard = () => {
   return (
       <div className={ "inwardsscore bg-blue-400 h-fit p-4 min-w-fit my-20 box"}>
        <div className="">
-        <h4 className="text-white p-2 text-xl font-mono">Score: {(score * options.level) + bonus.score * (options.level*2) } </h4>
+        <p className="text-white p-2 text-xl font-mono">Score: {(score * options.level) + bonus.score * (options.level*2) } </p>
         </div>
     
 
