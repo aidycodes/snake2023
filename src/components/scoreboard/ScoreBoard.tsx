acca8a670bdce7d01c8502eed5dc19b898e30c89
Aidy <aidycodes@gmail.com>
2023-02-12 21:04:33 +0000

    bug fixes

--- src/components/scoreboard/ScoreBoard.tsx
+++ src/components/scoreboard/ScoreBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import { SnakeContext } from '../../context/snakecontext'
 
 const ScoreBoard = () => {
@@ -8,7 +8,7 @@ const ScoreBoard = () => {
   return (
       <div className={ "inwardsscore bg-blue-400 h-fit p-4 min-w-fit my-20 box"}>
        <div className="">
-        <p className="text-white p-2 text-xl font-mono">Score: {(score * options.level) + bonus.score * (options.level*2) } </p>
+        <div className="text-white p-2 text-xl font-mono">Score: {(score * options.level) + bonus.score * (options.level*2) } </div>
         </div>
     
 
